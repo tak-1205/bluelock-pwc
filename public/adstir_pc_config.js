18c53a0d67e0041527160c1d7ac89ccb3640c45f
tak-1205 <225137807+tak-1205@users.noreply.github.com>
2025-10-13 16:08:27 +0900

    fix: adstirJSの許可

--- public/adstir_pc_config.js
+++ public/adstir_pc_config.js
@@ -1,6 +1,9 @@
 var adstir_vars = {
   ver: "4.0",
+  type: "banner",      // ← 明示
+  platform: "web",     // ← 明示
   app_id: "MEDIA-cf6de28",
   ad_spot: 2,
-  center: false
+  width: 300,          // ← PC 右カラム用
+  height: 250
 };
--- public/adstir_sp_config.js
+++ public/adstir_sp_config.js
@@ -1,6 +1,9 @@
 var adstir_vars = {
   ver: "4.0",
+  type: "banner",      // ← 明示
+  platform: "web",     // ← 明示
   app_id: "MEDIA-fd43f5bc",
-  ad_spot: 1,
-  center: false
+  ad_spot: 2,
+  width: 320,          // ← SP バナー
+  height: 50
 };
