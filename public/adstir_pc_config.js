a1fba9f602f8f6e4afedf02687ed14c08de18b36
tak-1205 <225137807+tak-1205@users.noreply.github.com>
2025-10-15 19:51:35 +0900

    fix: adstirレイアウト修正

--- public/adstir_pc_config.js
+++ public/adstir_pc_config.js
@@ -1,9 +1,6 @@
-window.adstir_vars = {
+var adstir_vars = {
   ver: "4.0",
-  type: "banner",
-  platform: "web",
   app_id: "MEDIA-cf6de28",
-  ad_spot: 1,
-  width: 300,
-  height: 250
+  ad_spot: 2,
+  center: false
 };
